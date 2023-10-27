ef0efc57485b4e9e28623df0353e984fb61a70d2
Eyadakn <eyadakn2017@gmail.com>
2023-10-27 01:35:30 -0500

    Finished GUI implemenation of log-in page

---
+++ GongChaPOS/src/logIn.tsx
@@ -0,0 +1,88 @@
+
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './logIn.css';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+    // Handle login logic here...
+  };
+
+  const togglePasswordVisibility = () => {
+    const passwordInput = document.getElementById("password") as HTMLInputElement;
+    if (passwordInput) {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+      } else {
+        passwordInput.type = "password";
+      }
+    }
+  };
+  
+
+  return (
+    <div className="container-fluid d-flex flex-row vh-100 p-0 background">
+      <div className="col-4 d-flex justify-content-center align-items-center vh-100 logoDiv">
+        <img
+          src="src/assets/images/GongChaLogo.png"
+          alt="GongCha Logo"
+          width="70%"
+          className="img-fluid"
+        />
+      </div>
+      <div className="col d-flex flex-column align-items-center vh-100 loginForm">
+        <div className="w-100 text-center my-2 loginText"> Log-In </div>
+        <form className="w-75 mt-4">
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label mb-1">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Please enter username here"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Please enter password here"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="checkbox"
+              id="showPassword"
+              className="form-check-input"
+              onChange={togglePasswordVisibility}
+            />
+            <label className="form-check-label" htmlFor="showPassword">Show Password</label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+
+
+
