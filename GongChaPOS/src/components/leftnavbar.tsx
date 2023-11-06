e6e449f0496c43c0fb3ebdbea5305f4877b1c179
ksvengurlekar-tamu <ksvengurlekar@tamu.edu>
2023-11-06 15:03:38 -0600

    nav bar changes + new component style sheet

--- GongChaPOS/src/components/leftnavbar.tsx
+++ GongChaPOS/src/components/leftnavbar.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
 function LeftNavBar() {
-
     return (
         <>
             <nav className='leftNavBar'>
-                
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='/cashierView'>Main Menu</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='/cashierView'>Recipes</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link' href='/cashierView'>Time Sheet</a>
+                    </li>
+                </ul>
             </nav>
         </>
-    )
+    );
 }
 
 export default LeftNavBar;
--- GongChaPOS/src/pages/cashierView.tsx
+++ GongChaPOS/src/pages/cashierView.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import LeftNavBar from '../components/leftnavbar.tsx'
+import '../components/components.css';
 
 function CashierView() {
   
   return (
-    <div style={{color: 'black'}}>
-        kiran
-    </div>
-    )
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3'>
+            <LeftNavBar />
+          </div>
+          <div className='col-md-9'>
+            show some menu item cards here
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default CashierView;
