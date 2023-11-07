b1bcdcabcb769bdb44bafb314350ff01c974dd56
Jedi1308 <jedidiah.samrajkumar@gmail.com>
2023-11-07 14:32:22 -0600

    Fixed some branching issues

--- GongChaPOS/src/components/card.tsx
+++ GongChaPOS/src/components/card.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-function card() {
-  return (
-    <div>card</div>
-  )
+interface CardDetails {
+  menuItemName: string;
 }
 
-export default card
+const generateRandomColor = (): string => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const Card: React.FC<CardDetails> = ({ menuItemName }) => {
+  const cardColor = generateRandomColor();
+
+  // Styling makes the card a square with a random color
+  const cardStyle = {
+    backgroundColor: cardColor,
+    width: '50px',
+    height: '50px',
+  };
+
+  return (
+    <div className="card" style={cardStyle}>
+      <h2>{menuItemName}</h2>
+      {/* Other card content */}
+    </div>
+  );
+};
+
+
+export default Card
