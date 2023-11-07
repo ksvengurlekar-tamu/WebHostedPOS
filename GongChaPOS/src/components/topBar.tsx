b145b0ebb4fddde60de2c1b47dc45c678d2eaf1b
Jedi1308 <jedidiah.samrajkumar@gmail.com>
2023-11-07 13:50:31 -0600

    Added TopBar with time functions

--- GongChaPOS/src/components/topBar.tsx
+++ GongChaPOS/src/components/topBar.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './TopBar.css';
 
-function topBar() {
+/**
+ * Renders the top bar component with a back button, title and current time.
+ * @returns JSX element
+ */
+function TopBar() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    // Update the time every second
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = currentTime.toLocaleTimeString();
+
   return (
-    <div>topBar</div>
-  )
+    <div className="top-bar">
+      <button className="back-button">Back</button>
+      {/* TODO: update top-bar-title to reflect tea series selected */}
+      <span className="top-bar-title">Bubble Tea Series</span>
+      <span className="top-bar-time">{formattedTime}</span>
+    </div>
+  );
 }
 
-export default topBar
+export default TopBar
--- GongChaPOS/src/pages/cashierView.tsx
+++ GongChaPOS/src/pages/cashierView.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import LeftNavBar from '../components/leftnavbar.tsx'
+import TopBar from '../components/topBar.tsx'
 import '../components/components.css';
 
 function CashierView() {
-  
+
   return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-3'>
-            <LeftNavBar />
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-3'>
+          <LeftNavBar />
+        </div>
+        <div className='col-md-12'>
+          <div>
+            <TopBar />
           </div>
           <div className='col-md-9'>
             show some menu item cards here
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default CashierView;
