33d25c4419f9cc8e2f2f70ff01b3d807e28df832
Jedi1308 <jedidiah.samrajkumar@gmail.com>
2023-11-30 00:36:50 -0600

    Weather discount is now also in customerView

--- GongChaPOS/src/pages/customerView.tsx
+++ GongChaPOS/src/pages/customerView.tsx
@@ -30,6 +30,7 @@ function CustomerView() {
     return saved === "true"; // If saved is the string 'true', return true, otherwise return false
   });
   const [series, setSeries] = useState("");
+  const [discounts, setDiscounts] = useState<string[]>([]);
   
   const view = "Customer View";
   const navigate = useNavigate();
@@ -41,13 +42,34 @@ function CustomerView() {
       updatedDrinks = [...JSON.parse(savedDrinks), ...drinks];
     }
     setDrinks(updatedDrinks);
+    if (discounts.length === 0) {
+      setDiscounts(determineDiscounts());
+    }
+    setDrinks(updatedDrinks);
   }, []);
 
-
+  const determineDiscounts = (): string[] => {
+    // Replace this with your actual logic
+    var discounts = ["First"];
+    axios.get('http://localhost:9000/weather/forecast')
+      .then(response => {
+        if (response.data.temperatureMin < 60.0) {
+          discounts.push('Cold');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+      });
+    return discounts;
+  }
 
   const addToCart = (drink: Drink): void => {
     if (drink.size === "Large") {
       drink.price += 0.75;
+    }
+
+    if (discounts.includes("Cold") && drink.name === "Caramel Chocolate Drink") {
+      drink.price = 0;
     }
 
     let updatedDrinks = [...drinks, drink];
