21446304858c09334a665b2ffb5ac3cbea8bb2ea
Rose Chakraborty <112028302+Supeena@users.noreply.github.com>
2023-12-04 22:39:03 -0600

    Documentation leftnavbar.tsx

--- GongChaPOS/src/components/leftnavbar.tsx
+++ GongChaPOS/src/components/leftnavbar.tsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
 
-
+/**
+ * LeftNavBar Component
+ *
+ * A navigation component representing the left sidebar of the application.
+ * It provides links to different views such as the main menu, nutritional facts,
+ * and inventory (for the Manager View).
+ *
+ * @component
+ *
+ * @param {Object} props - The properties of the LeftNavBar component.
+ * @param {string} props.view - The current view, which can be "Manager View" or "Cashier View".
+ *
+ * @returns {JSX.Element} The rendered LeftNavBar component.
+ */
 // view: input should be cashierView or managierView to set "main menu"'s path
 function LeftNavBar({ view }: { view: string }) {
 
