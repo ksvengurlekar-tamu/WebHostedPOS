d7043773f3368ab16a9dbeca52df0c463acb459d
Rose Chakraborty <112028302+Supeena@users.noreply.github.com>
2023-12-04 22:12:34 -0600

    Documentation googleLogIn.d.ts

--- GongChaPOS/src/components/googleLogIn.d.ts
+++ GongChaPOS/src/components/googleLogIn.d.ts
@@ -1,6 +1,28 @@
+/**
+ * GoogleLogInProps Interface
+ *
+ * Interface defining the props for the GoogleLogIn component.
+ *
+ * @interface
+ *
+ * @property {(googleUser: string) => void} onSignIn - Callback function triggered when a user successfully signs in with Google.
+ * @param {string} googleUser - The Google user information.
+ */
 interface GoogleLogInProps {
     onSignIn: (googleUser: string) => void;
 }
 
+/**
+ * GoogleLogIn Component
+ *
+ * A reusable component for handling Google sign-in.
+ *
+ * @component
+ *
+ * @param {GoogleLogInProps} props - The properties of the GoogleLogIn component.
+ * @param {Function} props.onSignIn - Callback function triggered when a user successfully signs in with Google.
+ *
+ * @returns {JSX.Element} The rendered GoogleLogIn component.
+ */
 declare const GoogleLogIn: React.FC<GoogleLogInProps>;
 export default GoogleLogIn;
