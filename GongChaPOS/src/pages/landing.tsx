58f44fc95296616faf58ba50763e7002fc9ef4a7
Jedi1308 <jedidiah.samrajkumar@gmail.com>
2023-12-04 20:12:20 -0600

    Fixed a single merge oopsie

--- GongChaPOS/src/pages/landing.tsx
+++ GongChaPOS/src/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import gongChaLogo from '../assets/images/GongChaLogo.png';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
